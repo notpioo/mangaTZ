90ec0c17ff33bb3de148e1c0ea9c15a81ef6e5e9
Avionika <afio12327@gmail.com>
2025-02-21 13:26:14 +0700

    f

--- public/js/config/firebase.js
+++ public/js/config/firebase.js
@@ -1,8 +1,7 @@
-// /public/js/config/firebase.js
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCszicewcL5M-nyhL3MXHJdjoObnVPK9Qg",
@@ -13,9 +12,6 @@ const firebaseConfig = {
   appId: "1:510033714996:web:39ed57a20169be5e25cf61"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
